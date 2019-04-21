46e96959943aaabcc73c5e8c6cf38dd394c72c94
quoctm <quoctm@vng.com.vn>
2019-04-21 15:48:26 +0700

    footer

---
+++ src/components/App/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './index.css';
+import {Footer} from "../Footer";
+
+const App = () => (
+  <Footer/>
+);
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./index.css";
+
+export const Footer = () => {
+	return (
+		<div className="footer">
+			<span>&copy; THE RULE BREAKER. DESIGNED BY STYLE MAKER STUDIO.</span>
+		</div>);
+};
